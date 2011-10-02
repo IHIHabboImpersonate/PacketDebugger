36beca3265be9dd449e6e4008ec087b763e62ac7
Chris Chenery <Cecer1@cecer1.com>
2011-10-02 21:57:23 +0100

    Restyled

--- Handlers.cs
+++ Handlers.cs
@@ -1,71 +1,210 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using IHI.Server.Habbos;
 using IHI.Server.WebAdmin;
+using IHI.Server.Networking;
+using IHI.Server.Networking.Messages;
+using Ion.Specialized.Encoding;
 
-namespace IHI.Server.Plugins.Cecer1.PluginManager
+namespace IHI.Server.Plugins.Cecer1.PacketDebugger
 {
     internal class Handlers
     {
+        Dictionary<uint, PacketHandler> fHandlers;
+        StringBuilder fPacketLogs;
+        const string HeaderSelect = "<input name='header' type='textbox'>"; //"<select><option value='0'>0 - @@</option><option value='1'>1 - @A</option><option value='2'>2 - @B</option></select>";
+
         Plugin fOwner;
         internal Handlers(Plugin Owner)
         {
             this.fOwner = Owner;
+            this.fHandlers = new Dictionary<uint, PacketHandler>();
+            this.fPacketLogs = new StringBuilder();
+
+            CoreManager.GetCore().GetConnectionManager().OnConnectionOpen += new ConnectionEventHandler(RegisterHandlersToNew);
         }
 
-        internal void PAGE_Index(HttpListenerContext Context)
+        internal void PAGE_List(HttpListenerContext Context)
         {
             if (Context.Request.IsLocal)
             {
-                CoreManager.GetCore().GetWebAdminManager().SendResponse(Context.Response, this.fOwner.GetName(), GetPluginList());
-            }
-        }
-        internal void PAGE_Toggle(HttpListenerContext Context)
-        {
-            if (Context.Request.IsLocal)
-            {
-                string Name = Context.Request.QueryString["name"];
+                StringBuilder Contents = new StringBuilder("<a href='packet/addform'>Add Handler</a><br><a href='packet/sendform'>Send Packet</a><br><a href='packet/logs'>View Logs</a><br><br>");
 
-                Plugin PluginFromName = CoreManager.GetCore().GetPluginManager().GetPlugin(Name);
-
-                GetPluginList();
-
-                if (PluginFromName == null)
+                lock (this.fHandlers)
                 {
-                    CoreManager.GetCore().GetWebAdminManager().SendResponse(Context.Response, this.fOwner.GetName(), "No such plugin (" + Name + ")<br><br>" + GetPluginList());
-                    return;
-                }
-                if (PluginFromName.IsRunning())
-                {
-                    if (Name == this.fOwner.GetName())
+                    foreach (uint HeaderID in this.fHandlers.Keys)
                     {
-                        CoreManager.GetCore().GetWebAdminManager().SendResponse(Context.Response, this.fOwner.GetName(), Name + " refuses to suicide! <br><br>" + GetPluginList());
-                        return;
+                        Contents.Append("<a href='remove?header=").Append(HeaderID).Append('>').Append(HeaderID).Append(" (").Append(Encoding.UTF8.GetString(Base64Encoding.EncodeuUInt32(HeaderID, 2))).Append(")</a><br>");
                     }
+                }
+                CoreManager.GetCore().GetWebAdminManager().SendResponse(Context.Response, this.fOwner.GetName(), Contents.ToString());
+            }
+        }
+        internal void PAGE_AddForm(HttpListenerContext Context)
+        {
+            if (Context.Request.IsLocal)
+            {
+                CoreManager.GetCore().GetWebAdminManager().SendResponse(Context.Response, this.fOwner.GetName(), "<form method='GET' action='add'><b>Header: </b>" + HeaderSelect + "<br><input type='submit' value='Go'></form>");
+            }
+        }
+        internal void PAGE_Add(HttpListenerContext Context)
+        {
+            if (Context.Request.IsLocal)
+            {
+                string Header = Context.Request.QueryString["header"];
 
-                    CoreManager.GetCore().GetPluginManager().StopPlugin(PluginFromName);
-                    CoreManager.GetCore().GetWebAdminManager().SendResponse(Context.Response, this.fOwner.GetName(), Name + " stopped<br><br>" + GetPluginList());
-                }
-                else
+                uint HeaderID = 0;
+
+                if (!uint.TryParse(Header, out HeaderID))
+                    HeaderID = Base64Encoding.DecodeUInt32(Encoding.UTF8.GetBytes(Header));
+
+             
+                PacketHandler Handler = new PacketHandler(Process_DYNAMIC);
+                lock(this.fHandlers)
+                    fHandlers.Add(HeaderID, Handler);
+                RegisterHandlersToExisting(HeaderID, Handler);
+
+                Context.Response.Redirect("addform");
+                Context.Response.Close();
+            }
+        }
+        internal void PAGE_Remove(HttpListenerContext Context)
+        {
+            if (Context.Request.IsLocal)
+            {
+                string Header = Context.Request.QueryString["header"];
+
+                uint HeaderID = 0;
+
+                if (!uint.TryParse(Header, out HeaderID))
+                    HeaderID = Base64Encoding.DecodeUInt32(Encoding.UTF8.GetBytes(Header));
+
+                UnregisterHandlers(HeaderID, this.fHandlers[HeaderID]);
+
+                lock (this.fHandlers)
+                    this.fHandlers.Remove(HeaderID);
+            }
+        }
+        internal void PAGE_SendForm(HttpListenerContext Context)
+        {
+            if (Context.Request.IsLocal)
+            {
+                StringBuilder Contents = new StringBuilder();
+
+                Contents.Append("<form method='GET' action='send'><b>Connection: </b><select name='connection'>");
+
+                foreach(IonTcpConnection Connection in CoreManager.GetCore().GetConnectionManager().GetAllConnections())
                 {
-                    CoreManager.GetCore().GetPluginManager().StartPlugin(PluginFromName);
-                    CoreManager.GetCore().GetWebAdminManager().SendResponse(Context.Response, this.fOwner.GetName(), Name + " started<br><br>" + GetPluginList());
+                    Contents.
+                        Append("<option value='").
+                        Append(Connection.GetID()).
+                        Append("'>").
+                        Append(Connection.GetID()).
+                        Append(" [").
+                        Append(Connection.GetIPAddressString()).
+                        Append("] ");
+
+                    Habbo ConnectionHabbo = Connection.GetHabbo();
+
+                    if(ConnectionHabbo != null && ConnectionHabbo.IsLoggedIn())
+                        Contents.Append(ConnectionHabbo.GetUsername());
+                    else
+                        Contents.Append("UNKNOWN");
+
+                    Contents.Append("</option>");
                 }
+
+                Contents.Append("</select><br><b>Header: </b>").Append(HeaderSelect).Append("<br><b>Data: </b><textarea name='data'></textarea><br><input type='submit' value='Go'></form>");
+
+                CoreManager.GetCore().GetWebAdminManager().SendResponse(Context.Response, this.fOwner.GetName(), Contents.ToString());
+            }
+        }
+        internal void PAGE_Send(HttpListenerContext Context)
+        {
+            if (Context.Request.IsLocal)
+            {
+                string Header = Context.Request.QueryString["header"];
+                string Data = Context.Request.QueryString["data"];
+                string SConnectionID = Context.Request.QueryString["connection"];
+
+                uint HeaderID = 0;
+                uint ConnectionID = 0;
+
+                if (!uint.TryParse(Header, out HeaderID))
+                    goto Done;
+                if (!uint.TryParse(SConnectionID, out ConnectionID))
+                    goto Done;
+
+                Habbo Habbo = CoreManager.GetCore().GetConnectionManager().GetConnection(ConnectionID).GetHabbo();
+
+                InternalOutgoingMessage Message = new InternalOutgoingMessage(HeaderID);
+                Message.Append(Data);
+                Habbo.SendMessage(Message);
+
+            Done:
+                Context.Response.Redirect("sendform");
+                Context.Response.Close();
+            }
+        }
+        internal void PAGE_Logs(HttpListenerContext Context)
+        {
+            if (Context.Request.IsLocal)
+            {
+                StringBuilder Contents = new StringBuilder("<html><head><style>.packet{border: 1px solid #000; margin: 5px; padding: 5px; background-color: #DDD;}</style></head><body>");
+
+                lock (this.fPacketLogs)
+                {
+                    Contents.Append(this.fPacketLogs.ToString());
+                    this.fPacketLogs.Clear();
+                }
+
+                Contents.Append("</body></html>");
+                CoreManager.GetCore().GetWebAdminManager().SendResponse(Context.Response, this.fOwner.GetName(), Contents.ToString());
             }
         }
 
-        private string GetPluginList()
+        private void Process_DYNAMIC(Habbo Sender, IncomingMessage Message)
         {
-            string PluginList = "<h1>Currently Loaded Plugins</h2><br>";
-
-            foreach (Plugin P in CoreManager.GetCore().GetPluginManager().GetLoadedPlugins())
+            lock (this.fPacketLogs)
             {
-                if (P.GetName() == this.fOwner.GetName())
-                    PluginList += "<a style='color: gray;' href='/admin/plugins/toggle?name=" + P.GetName() + "'><b>" + P.GetName() + "</b><br></a>";
-                else if (P.IsRunning())
-                    PluginList += "<a style='color: green;' href='/admin/plugins/toggle?name=" + P.GetName() + "'><b>" + P.GetName() + "</b><br></a>";
-                else
-                    PluginList += "<a style='color: red;' href='/admin/plugins/toggle?name=" + P.GetName() + "'><i>" + P.GetName() + "</i><br></a>";
+                this.fPacketLogs.
+                    Append("<div class='packet'>").
+                    Append('[').
+                    Append(Message.GetID()).
+                    Append("] <u>").
+                    Append(Message.GetHeader()).
+                    Append("</u>").
+                    Append(Message.GetContentString()).
+                    Append("</div>");
             }
-            return PluginList;
+        }
+
+        private void RegisterHandlersToNew(object source, ConnectionEventArgs args)
+        {
+            IonTcpConnection Connection = (source as IonTcpConnection);
+
+            lock (this.fHandlers)
+            {
+                foreach (KeyValuePair<uint, PacketHandler> Handler in this.fHandlers)
+                {
+                    Connection.AddHandler(Handler.Key, PacketHandlerPriority.High, Handler.Value);
+                }
+            }
+        }
+        private void RegisterHandlersToExisting(uint HeaderID, PacketHandler PacketHandler)
+        {
+            foreach (IonTcpConnection Connection in CoreManager.GetCore().GetConnectionManager().GetAllConnections())
+            {
+                Connection.AddHandler(HeaderID, PacketHandlerPriority.High, PacketHandler);
+            }
+        }
+        private void UnregisterHandlers(uint HeaderID, PacketHandler PacketHandler)
+        {
+            foreach (IonTcpConnection Connection in CoreManager.GetCore().GetConnectionManager().GetAllConnections())
+            {
+                Connection.RemoveHandler(HeaderID, PacketHandlerPriority.High, PacketHandler);
+            }
         }
     }
 }
---
+++ WEB_PacketDebugger.cs
@@ -0,0 +1,36 @@
+﻿using IHI.Server.Plugins;
+using IHI.Server.WebAdmin;
+
+namespace IHI.Server.Plugins.Cecer1.PacketDebugger
+{
+    public class WEB_PacketDebugger : Plugin
+    {
+        Handlers fHandlers;
+
+        public override void Start()
+        {
+            this.fHandlers = new Handlers(this);
+
+            CoreManager.GetCore().GetWebAdminManager().AddPathHandler("/admin/packet", new HttpPathHandler(this.fHandlers.PAGE_List));
+            CoreManager.GetCore().GetWebAdminManager().AddPathHandler("/admin/packet/add", new HttpPathHandler(this.fHandlers.PAGE_Add));
+            CoreManager.GetCore().GetWebAdminManager().AddPathHandler("/admin/packet/remove", new HttpPathHandler(this.fHandlers.PAGE_Remove));
+            CoreManager.GetCore().GetWebAdminManager().AddPathHandler("/admin/packet/send", new HttpPathHandler(this.fHandlers.PAGE_Send));
+            CoreManager.GetCore().GetWebAdminManager().AddPathHandler("/admin/packet/addform", new HttpPathHandler(this.fHandlers.PAGE_AddForm));
+            CoreManager.GetCore().GetWebAdminManager().AddPathHandler("/admin/packet/sendform", new HttpPathHandler(this.fHandlers.PAGE_SendForm));
+            CoreManager.GetCore().GetWebAdminManager().AddPathHandler("/admin/packet/logs", new HttpPathHandler(this.fHandlers.PAGE_Logs));
+        }
+
+        public override void Stop()
+        {
+            CoreManager.GetCore().GetWebAdminManager().RemovePathHandler("/admin/packet");
+            CoreManager.GetCore().GetWebAdminManager().RemovePathHandler("/admin/packet/add");
+            CoreManager.GetCore().GetWebAdminManager().RemovePathHandler("/admin/packet/remove");
+            CoreManager.GetCore().GetWebAdminManager().RemovePathHandler("/admin/packet/send");
+            CoreManager.GetCore().GetWebAdminManager().RemovePathHandler("/admin/packet/addform");
+            CoreManager.GetCore().GetWebAdminManager().RemovePathHandler("/admin/packet/sendform");
+            CoreManager.GetCore().GetWebAdminManager().RemovePathHandler("/admin/packet/logs");
+
+            this.fHandlers = null;
+        }
+    }
+}
